0a7c53cf90c07a8bb04561e31e1f49347a297654
caezar47 <iss47@ya.ru>
2017-01-23 16:22:58 +0300

    новые правки очередные аааааа

---
+++ src/block/site/page/portfolio-item/.window-resize.js
@@ -0,0 +1,11 @@
+/*var h_img = $(".lSSlideWrapper").outerHeight(true),
+	w_img = $("._pipc__cols-note").outerWidth(true),
+	h_soc = $("._pipc__row-slider").outerHeight(true),
+	b_top = h_soc + 10,
+	b_bottom = h_soc + h_img + 5;
+	$("._pipc__slider-border.top").css("width", w_img); 
+
+$("._pipc__slider-border.top").css("top", b_top); 
+$("._pipc__slider-border.right").css("top", b_top); 
+$("._pipc__slider-border.left").css("top", b_top); 
+$("._pipc__slider-border.bottom").css("top", b_bottom); */
